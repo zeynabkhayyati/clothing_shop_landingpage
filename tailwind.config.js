3321b9820643ffac6865b36417b9c7bb37662acf
zeynab <zeynab.khayyati.11@gmail.com>
2023-09-11 18:19:29 +0330

    make navbar and main

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./*.{html,js}"],
+        theme: {
+        extend: {
+            colors:{
+                'greenBackground':'#c2efd4',
+                'greenCart':'#6bc785',
+                'greenText':'#224f34'
+            }
+        },
+    },
+    plugins: [],
+}
